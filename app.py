c1d80663504d626f52b7ee0d3b4509174699189d
Uttam maurya <ummauryas2005@gmail.com>
2026-03-25 01:25:40 +0530

    correct  the error

--- app.py
+++ app.py
@@ -122,5 +122,5 @@ def internal_error(e):
 # 🧠 START SERVER
 # ===============================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
