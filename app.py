ca094c493e7c741bc2e4b8cbec5346ef28232774
Uttam maurya <ummauryas2005@gmail.com>
2026-03-25 01:17:09 +0530

    correct the error

--- app.py
+++ app.py
@@ -122,6 +122,5 @@ def internal_error(e):
 # 🧠 START SERVER
 # ===============================
 if __name__ == "__main__":
-    print("🚀 Starting CareSync Flask Server...")
-    print("🔗 Visit: http://127.0.0.1:5000/")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
